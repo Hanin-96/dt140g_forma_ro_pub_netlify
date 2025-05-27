ab2a1e28a37c40497421ad48edff2e905cebbef2
Hanin Farhan <haninfarhan96@gmail.com>
2025-05-27 15:05:43 +0200

    Testar optimera produktsidans bilder

--- src/pages/Ceramic/ProductPage.tsx
+++ src/pages/Ceramic/ProductPage.tsx
@@ -44,7 +44,7 @@ const ProductPage = observer(() => {
                             <p className="mt-10">{productStore.product.product_description}</p>
                         </div>
                         <div className="aspect-square w-full max-w-[50rem]">
-                            <img src={productStore.product.product_thumbnail} alt={productStore.product.product_thumbnail_alt} className="object-cover w-full rounded-2xl shadow-sm h-full" />
+                            <img src={productStore.product.product_thumbnail} height={500} width={500} alt={productStore.product.product_thumbnail_alt} className="object-cover w-full rounded-2xl shadow-sm h-full" />
                         </div>
                     </section>
 
@@ -58,7 +58,7 @@ const ProductPage = observer(() => {
 
                                 <Link key={product.id} to={`/keramik-produkt/${product.id}`} className="hover:no-underline max-w-[30rem] h-full w-full">
                                     <article className="border-[1px] border-forma_ro_grey rounded-3xl text-center max-w-[30rem] h-full w-full hover:bg-forma_ro_red">
-                                        <img src={product.product_thumbnail} alt={product.product_thumbnail_alt} className="rounded-t-2xl max-h-[30rem] max-w-[30rem] h-full w-full object-cover min-h-[30rem]" />
+                                        <img src={product.product_thumbnail} height={300} width={300} alt={product.product_thumbnail_alt} className="rounded-t-2xl max-h-[30rem] max-w-[30rem] h-full w-full object-cover min-h-[30rem]" />
                                         <h4 className="text-[20px] p-2 font-semibold">{product.title.rendered}</h4>
                                         <p className="p-2">{product.product_price}:-</p>
                                         <button className="flex gap-1 justify-center mx-auto p-2 text-[18px]">Se produkt  <ChevronsRight className="color-forma_ro_black" /></button>
